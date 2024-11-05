8d8d8aa2317e0b5dc5be660d8f3eac7aa8eb915a
searj15 <sjbventurina@unionbankph.com>
2024-11-05 16:47:55 +0800

    v1 simple portfolio page

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,92 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { Card } from '@/components/ui/card';
+import { SiGmail } from 'react-icons/si';
+import { FaLinkedin } from 'react-icons/fa6';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='flex flex-col justify-center items-center gap-16 md:32 p-8 pt-32 lg:p-32 xl:p-64'>
+      
+      <section id='home'>
+        <div className='flex flex-col md:flex-row items-center gap-4'>
+          <div className='text-center'>
+            <h1 className='font-bold text-xl md:text-2xl'>{`Hi! I'm Sear John Venturina`}</h1>
+            <p className='text-md md:text-xl'>Software Engineer</p>
+          </div>
+          <Image
+            className='rounded-full'
+            src='https://lh3.googleusercontent.com/a/ACg8ocKiwO6eQdDsD5ahXVgzgAA6QjHdcYm9-V9qVoJ14Qe_nHIeoUc=s288-c-no'
+            alt={`Searj's picture`}
+            width={200}
+            height={200}
+            priority
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+
+      <section id='aboutme'>
+        <div className='grid gap-4 text-center'>
+          <h1 className='font-bold text-2xl'>About Me</h1>
+          <div className='grid md:grid-cols-2 md:flex-row justify-center items-center gap-4'>
+            <Card className='flex flex-col items-center justify-center p-8'>
+              <Image
+                className='rounded-full'
+                src='https://images.credly.com/images/b9feab85-1a43-4f6c-99a5-631b88d5461b/image.png'
+                alt={`Searj's certificate`}
+                width={150}
+                height={150}
+                priority
+              />
+              <h4 className='text-xl font-bold'>Certification</h4>
+              <p className='text-md'>AWS Certified Associate Developer</p>
+            </Card>
+
+            <Card className='flex flex-col items-center justify-center p-8'>
+              <Image
+                className='rounded-full'
+                src='https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png'
+                alt={`Searj's technologies`}
+                width={150}
+                height={150}
+                priority
+              />
+              <h4 className='text-xl font-bold'>Framework</h4>
+              <p className='text-md'>6+ years ReactJS Development</p>
+            </Card>
+          </div>
+          <p className='text-justify'>
+            A senior front-end developer in ReactJS and Banking sector expert
+            with 6 years of experience for writing, code reviewing and
+            documenting codes using latest technologies to improve software
+            quality. My job is very grounded in front and back-end development,
+            but I was reasonably focused to makethesystem secure, maintainable
+            and user-friendly. I have experience designing, implementing, and
+            optimizing sophisticated web applications that not only satisfy
+            business requirements but also comply with regulatory guidelines.
+          </p>
+        </div>
+      </section>
+
+      <section id='contactme'>
+        <div className='grid gap-4 text-center'>
+          <h1 className='font-bold text-2xl'>Contact Me</h1>
+          <Card className='flex flex-col md:flex-row gap-4 p-8'>
+            <div className='flex gap-2 items-center'>
+              <SiGmail size={20} />
+              <a href='mailto:venturinasearjohn@gmail.com'>
+                venturinasearjohn@gmail.com
+              </a>
+            </div>
+
+            <div className='flex md:flex-row gap-2 items-center'>
+              <FaLinkedin size={20} />
+              <a href='https://www.linkedin.com/in/sear-john-venturina-6b411813a/'>
+                Sear John Venturina
+              </a>
+            </div>
+          </Card>
+        </div>
+      </section>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['lh3.googleusercontent.com', 'images.credly.com', 'pluspng.com'],
+    },
+  };
+  
+  export default nextConfig;
+  
